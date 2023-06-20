e067baa002ded3443c35bec70136c50547a3cfaa
Mikołaj Pieczaba <mikolaj@pieczaba.me>
2023-06-20 18:11:32 +0200

    fix: add missing files

--- frontend/src/ts/lib/model/UnitResponse.ts
+++ frontend/src/ts/lib/model/UnitResponse.ts
@@ -2,12 +2,20 @@ import Position from "./Position.js";
 
 export default class UnitResponse {
   public id: string;
+  public range: number;
   public hp: number;
   public damage: number;
   public position: Position;
 
-  constructor(id: string, hp: number, damage: number, position: Position) {
+  constructor(
+    id: string,
+    range: number,
+    hp: number,
+    damage: number,
+    position: Position
+  ) {
     this.id = id;
+    this.range = range;
     this.hp = hp;
     this.damage = damage;
     this.position = position;
