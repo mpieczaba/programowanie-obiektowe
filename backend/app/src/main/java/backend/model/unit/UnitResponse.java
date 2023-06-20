5eb97de830dc647747306de881adad879132cc9b
Mikołaj Pieczaba <mikolaj@pieczaba.me>
2023-06-20 18:07:14 +0200

    feat: add range to unit response and add some new sprites

--- backend/app/src/main/java/backend/model/unit/UnitResponse.java
+++ backend/app/src/main/java/backend/model/unit/UnitResponse.java
@@ -4,10 +4,12 @@ import backend.model.entity.EntityResponse;
 
 public class UnitResponse extends EntityResponse {
     public int damage;
+    public int range;
 
     public UnitResponse(Unit unit) {
         super(unit);
 
         this.damage = unit.damage;
+        this.range = unit.range;
     }
 }
